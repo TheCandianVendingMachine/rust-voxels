5ef4af7497867e1341389fccdc877777d20fa8cc
Bailey Danyluk <baileydanyluk@gmail.com>
2023-09-04 20:23:47 -0600

    Refactor pipeline creation
    There was an un-needed function that just separated code for no real
    reason. Consolodate

--- src/render_graph/compiled_graph.rs
+++ src/render_graph/compiled_graph.rs
@@ -55,14 +55,14 @@ impl<'graph> CompiledGraph<'graph> {
         conservative: false
     };
 
-    pub fn compile_from_definition<'compile_graph, 'device: 'compile_graph, S>(
-        graph: &'compile_graph super::RenderGraph,
-        device: &'device wgpu::Device,
-        shaders: HashMap<ShaderHandle, &ShaderBuilder<'compile_graph, S>>,
-        vertex_buffer_layout: &'compile_graph [wgpu::VertexBufferLayout],
-        colour_target_state: &'compile_graph [Option<wgpu::ColorTargetState>]
-    ) -> CompiledGraph<'compile_graph> where
-        S: Clone + std::fmt::Debug + ShaderSource<'compile_graph> {
+    pub fn compile_from_definition<S>(
+        graph: &'graph super::RenderGraph,
+        device: &wgpu::Device,
+        shaders: HashMap<ShaderHandle, &ShaderBuilder<'graph, S>>,
+        vertex_buffer_layout: &'graph [wgpu::VertexBufferLayout],
+        colour_target_state: &'graph [Option<wgpu::ColorTargetState>]
+    ) -> CompiledGraph<'graph> where
+        S: Clone + std::fmt::Debug + ShaderSource<'graph> {
         /* Algorithm:
          * 1. Reverse directions and perform topological sort on graph
          * 2. From topological sort, if the resource is not an external dependency, create
@@ -86,15 +86,15 @@ impl<'graph> CompiledGraph<'graph> {
                 },
                 Vertex::Blue(pass_handle) => {
                     let pass = graph.passes.get_from_handle(pass_handle).unwrap();
-                    let pipeline = graph.pipelines.get_from_handle(&pass.pipeline).unwrap();
+                    let pipeline_info = graph.pipelines.get_from_handle(&pass.pipeline).unwrap();
                     if !pipeline_layouts.contains_key(&pass.pipeline) {
-                        pipeline_layouts.insert(pass.pipeline, pipeline.builder.clone().build());
+                        pipeline_layouts.insert(pass.pipeline, pipeline_info.builder.clone().build());
                     }
                     let pipeline_layout = pipeline_layouts.get_mut(&pass.pipeline).unwrap();
                     // Create wgpu pipeline if it doesnt exist already
                     compiled_graph.create_pipeline(
                         pass,
-                        pipeline,
+                        pipeline_info,
                         pipeline_layout,
                         device,
                         &shaders,
@@ -103,7 +103,7 @@ impl<'graph> CompiledGraph<'graph> {
                     );
 
                     // Create render pass from pipeline
-                    compiled_graph.create_render_pass();
+                    compiled_graph.create_render_pass(device, pass.pipeline.uuid());
                 },
             }
         }
@@ -111,25 +111,54 @@ impl<'graph> CompiledGraph<'graph> {
         compiled_graph
     }
 
-    fn create_render_pass(
-        &mut self
+    fn create_render_pass<'render_pass>(
+        &'render_pass mut self,
+        device: &wgpu::Device,
+        pipeline_id: Uuid
     ) {
+        let pipeline = self.render_pipelines.get(&pipeline_id).unwrap();
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder")
+        });
+
+        {
+            /*let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0
+                        }),
+                        store: true
+                    }
+                })],
+                depth_stencil_attachment: None
+            });
+
+            render_pass.set_pipeline(&pipeline);
+            render_pass.draw(0..3, 0..1);*/
+        }
     }
 
-    fn create_pipeline<'pipeline, S>(
-        &'pipeline mut self,
+    fn create_pipeline<S>(
+        &mut self,
         pass_builder: &RenderPassBuilder,
         pipeline_info: &PipelineInfo,
         pipeline_layout: &mut render::PipelineLayout<'graph>,
-        device: &'graph wgpu::Device,
+        device: &wgpu::Device,
         shaders: &HashMap<ShaderHandle, &ShaderBuilder<'graph, S>>,
         vertex_buffer_layout: &'graph [wgpu::VertexBufferLayout],
         colour_target_state: &'graph [Option<wgpu::ColorTargetState>]
-    ) -> &'pipeline RenderPipeline where
+    ) where
         S: Clone + std::fmt::Debug + ShaderSource<'graph>,
     {
         if !self.render_pipelines.contains_key(&pass_builder.pipeline.uuid()) {
-            return self.render_pipelines.get(&pass_builder.pipeline.uuid()).unwrap()
+            return
         }
 
         let vertex_shader = ShaderData {
@@ -152,90 +181,63 @@ impl<'graph> CompiledGraph<'graph> {
             }
         );
 
-        self.add_render_pipeline(
-            device,
-            pass_builder.pipeline.uuid(),
-            Some(ResourcePair::new(pass_builder.pipeline.uuid(), pipeline_layout)),
-            vertex_shader,
-            fragment_shader
-        )
-    }
-
-    fn add_render_pipeline<'pipeline, VS, FS>(
-        &'pipeline mut self,
-        device: &wgpu::Device,
-        render_pipeline_id: Uuid,
-        mut render_pipeline_layout_builder: Option<ResourcePair<&mut render::PipelineLayout<'graph>>>,
-        vertex_shader_builder: ShaderData<'graph, wgpu::VertexBufferLayout, VS>,
-        fragment_shader_builder: Option<ShaderData<'graph, Option<wgpu::ColorTargetState>, FS>>,
-    ) -> &'pipeline RenderPipeline where
-        VS: ShaderSource<'graph> + std::fmt::Debug + Clone,
-        FS: ShaderSource<'graph> + std::fmt::Debug + Clone,
-    {
-        if !self.render_pipelines.contains_key(&render_pipeline_id) {
-            if !self.shaders.contains_key(&vertex_shader_builder.module_builder.id) {
-                self.shaders.insert(
-                    vertex_shader_builder.module_builder.id,
-                    device.create_shader_module(vertex_shader_builder.module_builder.resource.build())
-                );
-            }
-
-            if let Some(fs) = &fragment_shader_builder {
-                if !self.shaders.contains_key(&fs.module_builder.id) {
-                    self.shaders.insert(
-                        fs.module_builder.id,
-                        device.create_shader_module(fs.module_builder.resource.build())
-                    );
-                }
-            }
-
-            if let Some(layout) = &mut render_pipeline_layout_builder {
-                if !self.pipeline_layouts.contains_key(&layout.id) {
-                    self.pipeline_layouts.insert(
-                        layout.id,
-                        layout.resource.create(&device)
-                    );
-                }
-            }
-
-            let vertex_shader = self.shaders.get(&vertex_shader_builder.module_builder.id).unwrap();
-            let fragment_shader = fragment_shader_builder.as_ref().map(
-                |b| self.shaders.get(&b.module_builder.id).unwrap()
-            );
-            let pipeline_layout = render_pipeline_layout_builder.map(
-                |b| self.pipeline_layouts.get(&b.id).unwrap()
-            );
-
-            let render_pipeline_descriptor = wgpu::RenderPipelineDescriptor {
-                label: None,
-                layout: pipeline_layout,
-                vertex: wgpu::VertexState {
-                    module: &vertex_shader,
-                    entry_point: Self::VERTEX_SHADER_ENTRY,
-                    buffers: vertex_shader_builder.inputs
-                },
-                fragment: fragment_shader.map(|fs|
-                    wgpu::FragmentState {
-                        module: &fs,
-                        entry_point: Self::FRAGMENT_SHADER_ENTRY,
-                        targets: fragment_shader_builder.unwrap().inputs,
-                    },
-                ),
-                primitive: Self::PRIMITIVE_STATE,
-                depth_stencil: None,
-                multisample: wgpu::MultisampleState {
-                    count: 1,
-                    mask: !0,
-                    alpha_to_coverage_enabled: false
-                },
-                multiview: None
-            };
-
-            self.render_pipelines.insert(
-                render_pipeline_id,
-                device.create_render_pipeline(&render_pipeline_descriptor)
+        if !self.shaders.contains_key(&vertex_shader.module_builder.id) {
+            self.shaders.insert(
+                vertex_shader.module_builder.id,
+                device.create_shader_module(vertex_shader.module_builder.resource.build())
             );
         }
-        self.render_pipelines.get(&render_pipeline_id).unwrap()
+
+        if let Some(fs) = &fragment_shader {
+            if !self.shaders.contains_key(&fs.module_builder.id) {
+                self.shaders.insert(
+                    fs.module_builder.id,
+                    device.create_shader_module(fs.module_builder.resource.build())
+                );
+            }
+        }
+
+        if !self.pipeline_layouts.contains_key(&pass_builder.pipeline.uuid()) {
+            self.pipeline_layouts.insert(
+                pass_builder.pipeline.uuid(),
+                pipeline_layout.create(&device)
+            );
+        }
+
+        let vertex_shader_module = self.shaders.get(&vertex_shader.module_builder.id).unwrap();
+        let fragment_shader_module = fragment_shader.as_ref().map(
+            |b| self.shaders.get(&b.module_builder.id).unwrap()
+        );
+        let pipeline_layout = self.pipeline_layouts.get(&pass_builder.pipeline.uuid()).unwrap();
+
+        let render_pipeline_descriptor = wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: Some(pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &vertex_shader_module,
+                entry_point: Self::VERTEX_SHADER_ENTRY,
+                buffers: vertex_shader.inputs
+            },
+            fragment: fragment_shader_module.map(|fs|
+                wgpu::FragmentState {
+                    module: &fs,
+                    entry_point: Self::FRAGMENT_SHADER_ENTRY,
+                    targets: fragment_shader.unwrap().inputs,
+                },
+            ),
+            primitive: Self::PRIMITIVE_STATE,
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false
+            },
+            multiview: None
+        };
+
+        self.render_pipelines.insert(
+            pass_builder.pipeline.uuid(),
+            device.create_render_pipeline(&render_pipeline_descriptor)
+        );
     }
 }
